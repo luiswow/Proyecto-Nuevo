c304e7fedc7737e8da38c540767416eca38d5da1
Luis Reyes <luisjavier_iza@hotmail.com>
2020-04-01 17:54:48 -0600

    Primer Commit

---
+++ src/main/java/com/chivisgt/SpringFacturaApplication.java
@@ -0,0 +1,13 @@
+package com.chivisgt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFacturaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFacturaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chivisgt/SpringFacturaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chivisgt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFacturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

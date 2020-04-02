ff103a70a223bd02cf27a66eb90cf66450f656bf
Luis Reyes <luisjavier_iza@hotmail.com>
2020-04-01 18:06:04 -0600

    Se agrego la entidad, dao y la interfaz

--- src/main/java/com/chivisgt/models/entity/Cliente.java
+++ src/main/java/com/chivisgt/models/entity/Cliente.java
@@ -4,7 +4,7 @@ import java.io.Serializable;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
+:
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
@@ -16,6 +16,7 @@ public class Cliente implements Serializable {
 	private static final long serialVersionUID = 1L;
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue
 	private Long id;
 	@NotEmpty
 	private String nombre;

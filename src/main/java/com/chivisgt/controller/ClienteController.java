fc69aa9baa916fd80de71b5144b8f3413be8db05
luis <luisjavier_iza@hotmail.com>
2020-04-02 22:05:20 -0600

    Se agregaron metodos al controlador

--- src/main/java/com/chivisgt/controller/ClienteController.java
+++ src/main/java/com/chivisgt/controller/ClienteController.java
@@ -1,14 +1,23 @@
 package com.chivisgt.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 import com.chivisgt.models.entity.Cliente;
 import com.chivisgt.models.service.IClienteService;
 
 @Controller
+@SessionAttributes("cliente")
 public class ClienteController {
 	@Autowired
 	private IClienteService clienteService;
@@ -26,6 +35,46 @@ public class ClienteController {
 		model.addAttribute("titulo", "Crear Cliente");
 		return"form";
 
+	}
+	
+	@RequestMapping(value="/form",method=RequestMethod.POST)
+	public String guardar(@Valid Cliente cliente,BindingResult result, SessionStatus status, Model model ) {
+		
+		if(result.hasErrors()) {
+	  model.addAttribute("titulo", "Crear Cliente");
+	  return"/form";
+			
+			
+		}
+		clienteService.save(cliente);
+		status.setComplete();
+		return "listar";
+	}
+	@RequestMapping("/form/{id}")
+	public String editar(@PathVariable (value="id") Long id,Model model) {
+		Cliente cliente = new Cliente();
+		if(id>0) {
+			cliente = clienteService.findOne(id);
+			model.addAttribute("cliente", cliente);
+			
+			
+			
+		}else {
+			
+			return"/listar";
+		}
+		
+		return "form";
+		
+		
+	}
+	@RequestMapping("/delete/{id}")
+	public String eliminar(@PathVariable(value="id")Long id,Model model) {
+	if(id>0) {
+		clienteService.delete(id);
+			
+		}
+		return"redirect:/listar";
 	}
 
 }

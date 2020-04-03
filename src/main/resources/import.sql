fd6bc0c8d216ed1538e1951697226b1d6fd29d36
luiswow <58117229+luiswow@users.noreply.github.com>
2020-04-02 22:12:30 -0600

    solo agregar el ultimo commit  (#5)
    * Se agregaron metodos al controlador

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,1 +1,5 @@
-insert into clientes (nombre,apellido,email) VALUES('luis','reyes','luisjavier_iza@hotmail.com')
+insert into clientes (nombre,apellido,email) VALUES('luis','reyes','luisjavier_iza@hotmail.com');
+insert into clientes (nombre,apellido,email) VALUES('pedro','reyes','luisjavier_iza@hotmail.com');
+insert into clientes (nombre,apellido,email) VALUES('maria','reyes','luisjavier_iza@hotmail.com');
+insert into clientes (nombre,apellido,email) VALUES('valentin','reyes','luisjavier_iza@hotmail.com');
+insert into clientes (nombre,apellido,email) VALUES('sanyoni','reyes','luisjavier_iza@hotmail.com')
